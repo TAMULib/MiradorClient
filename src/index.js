7b9aef8acdc4a0e09d523b8636a7bb78d9c87b4d
William Welling <wwelling@library.tamu.edu>
2021-03-30 10:28:18 -0500

    test local storage annotations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import Mirador from 'mirador/dist/es/src/index';
+import miradorAnnotationPlugins from 'mirador-annotations';
+import LocalStorageAdapter from 'mirador-annotations/lib/LocalStorageAdapter';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import Mirador from 'mirador/dist/es/src/index';
 
 const config = {
   id: 'viewer',
+  annotation: {
+    adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`)
+  },
   windows: [{
+    defaultSideBarPanel: 'annotations',
+    sideBarOpenByDefault: true,
     imageToolsEnabled: true,
     imageToolsOpen: true,
     manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
@@ -12,11 +19,12 @@ const config = {
     palette: {
       primary: {
         main: '#1967d2',
-      },
-    },
-  },
+      }
+    }
+  }
 };
 
 Mirador.viewer(config, [
   ...miradorImageToolsPlugin,
+  ...miradorAnnotationPlugins
 ]);
